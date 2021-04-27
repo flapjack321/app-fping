dd0d8a32c1c9d89af299fefef4fdeb29ec24c9e4
Ethan Cotterell <e.cotterell@lancaster.ac.uk>
2021-04-27 12:24:13 +0000

    run.sh: fixed missing quotations

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-sudo qemu-guest -k build/fping_kvm-x86_64 -m 64 -b kraft0 -a netdev.ipv4_addr=172.88.0.2 netdev.ipv4_gw_addr=172.88.0.1 netdev.ipv4_subnet_mask=255.255.255.0 --
+#!/bin/bash
+sudo qemu-guest \
+    -k build/fping_kvm-x86_64 \
+    -m 64 \
+    -b kraft0 \
+    -a "netdev.ipv4_addr=172.88.0.2 netdev.ipv4_gw_addr=172.88.0.1 netdev.ipv4_subnet_mask=255.255.255.0 --"
